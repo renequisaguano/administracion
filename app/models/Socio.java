8875cd6aa73e4f7374c0c7869e2b70d100e7a57c
Your Name <you@example.com>
2015-03-30 17:48:21 -0500

    Agregando validaciones en Socios

--- app/models/Socio.java
+++ app/models/Socio.java
@@ -1,8 +1,10 @@
 package models;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.Pattern;
 
+import net.sf.oval.constraint.MinLength;
+import play.data.validation.Match;
+import play.data.validation.MaxSize;
 import play.data.validation.Required;
 import play.data.validation.Unique;
 import play.db.jpa.Blob;
@@ -11,14 +13,27 @@ import play.db.jpa.Model;
 @Entity
 public class Socio extends Model {
 	
-	@Pattern(regexp = "^[\\p{L} .'-]+$")
-	@Required(message="Ingrese los nombres")
-	public String nombre;
-	@Required(message="Ingrese los apellidos")
-	public String apellido;
-	@Required(message="Ingrese el numero de cedula")
+	
+	@Match(value="[0-9]+$",message="Ingrese solo números")
+	@Required(message="Ingrese el número de cedula")
+	@MaxSize(value=10,message="La cédula no puede tener mas de 10 dígitos")
+	@MinLength(value=10,message="La cédula no puede tener menos de 10 dígitos")
 	@Unique(message="Esta cedula ya ha sido Registrada Anteriormente")
 	public String cedula;
+	
+	
+	@Required(message="Ingrese los apellidos")
+	@Match(value="[áéíóúÁÉÍÓÚñÑA-Za-z ]+$",message="Ingrese solo letras")
+	public String apellido;
+	
+
+	@Match(value="[áéíóúÁÉÍÓÚñÑA-Za-z ]+$",message="Ingrese solo letras")
+	@Required(message="Ingrese los nombres")
+	public String nombre;
+	
+	
+
+	
 	public Blob foto;
 	
 	public String toString(){
