b1fa346618ca32ff1687d81ae244f29d71d8fe6b
Your Name <you@example.com>
2015-03-30 13:20:57 -0500

    Vinculado seguridad a los controladores

--- app/controllers/Socios.java
+++ app/controllers/Socios.java
@@ -1,10 +1,25 @@
 package controllers;
 
 import models.Socio;
-
+import models.Usuario;
+import play.mvc.Before;
+import play.mvc.With;
+@With(Secure.class)
 public class Socios extends CRUD {
 
- 
+	@Before
+	public static void mostrarUsuario(){
+		try{
+			Usuario user = Usuario.find("byEmail", Security.connected()).first();
+			//obteniendo datos del usuario que ha iniciado sesion
+			if(Security.isConnected()) {
+		        renderArgs.put("conectado", user.nombre+" "+user.apellido); 
+		    }
+			}catch(Exception ex){
+				
+			}
+	}
+	
 	 public static void verFoto(Long id){
 	    	Socio s=Socio.findById(id);
 	    	  response.setContentTypeIfNotSet(s.foto.type());
--- app/models/Socio.java
+++ app/models/Socio.java
@@ -1,16 +1,20 @@
 package models;
 
-import play.*;
-import play.db.jpa.*;
+import javax.persistence.Entity;
 
-import javax.persistence.*;
-
-import java.util.*;
+import play.data.validation.Required;
+import play.data.validation.Unique;
+import play.db.jpa.Blob;
+import play.db.jpa.Model;
 
 @Entity
 public class Socio extends Model {
+	@Required(message="Ingrese los nombres")
 	public String nombre;
+	@Required(message="Ingrese los apellidos")
 	public String apellido;
+	@Required(message="Ingrese el numero de cedula")
+	@Unique(message="Esta cedula ya ha sido Registrada Anteriormente")
 	public String cedula;
 	public Blob foto;
 	
